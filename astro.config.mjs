fc711a6bef01b43788173b067d6ec7bbf3337c20
MwjEnde <maarten.w.j@gmail.com>
2026-01-04 13:41:44 +0100

    Configure base URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://MwjEnde.github.io',
+  base: 'floris_site',
   vite: {
     plugins: [tailwindcss()]
   }
